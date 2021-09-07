f4a2826e65d5e20d47cf8f69218e3581f8ff5c1c
Elliot Heiner <elliotheiner1997@gmail.com>
2021-09-07 08:57:57 -0600

    Add loop examples

--- LoopExample/LoopExamples.vb
+++ LoopExample/LoopExamples.vb
@@ -1,7 +1,24 @@
-﻿Module LoopExamples
+﻿Option Explicit On
+
+Module LoopExamples
 
     Sub Main()
+        'Dim pattern As String
+        'For i = 1 To 10
+        '    'Console.WriteLine(i)
+        '    pattern &= "*"
+        '    Console.WriteLine(pattern)
+        'Next
 
+        Dim count As Integer '= 11
+        'Do While count <= 10
+        '    count += 1
+        'Loop
+
+        Do
+            count += 1
+        Loop While count <= 10
+        Console.Read()
     End Sub
 
 End Module
